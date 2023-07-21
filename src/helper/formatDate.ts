9ed0ef5d7e92a538ca705322cef311c98aec0097
Gustavo Fonseca <gusfonseca.dev@gmail.com>
2023-07-21 20:46:51 -0300

    Erro formatData

--- src/helper/formatDate.ts
+++ src/helper/formatDate.ts
@@ -1,31 +1,10 @@
-// export const formatDate = (date: string) => {
-//   console.log("date", date);
-//   const newDate = new Date(date);
-//   console.log("newDate", newDate);
-
-//   const formatter = new Intl.DateTimeFormat("pt-BR", {
-//     month: "long",
-//     year: "numeric",
-//   });
-
-//   return formatter.format(newDate);
-// };
-
 export const formatDate = (date: string) => {
-  try {
-    const newDate = new Date(date);
-    if (isNaN(newDate.getTime())) {
-      throw new Error("Invalid date format");
-    }
+  const newDate = new Date(`${date}-01`);
 
-    const formatter = new Intl.DateTimeFormat("pt-BR", {
-      month: "long",
-      year: "numeric",
-    });
+  const formatter = new Intl.DateTimeFormat("pt-BR", {
+    month: "long",
+    year: "numeric",
+  });
 
-    return formatter.format(newDate);
-  } catch (error: any) {
-    console.error("Error formatting date:", error.message);
-    return "Invalid Date";
-  }
+  return formatter.format(newDate);
 };
